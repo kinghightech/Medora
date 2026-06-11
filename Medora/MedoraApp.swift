13c8f96ca0aa4854d8eeace91eb75a148391c38a
Aahish Abbani <shibe.aahish@gmail.com>
2026-06-11 15:27:50 -0400

    Initial Commit

---
+++ Medora/MedoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedoraApp.swift
+//  Medora
+//
+//  Created by Aahish Abbani on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MedoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
